8dbc827a6911a92d4d84e2674ce912fefe9f3477
ArmandoPaPa <70594270+ArmandoPaPa@users.noreply.github.com>
2020-10-02 16:10:31 +0300

    CV webpage v01

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+
+/*
+document.getElementsByClassName("sect_Container sc_left jsLeft").addEventListener("click", function(){
+     document.getElementsByClassName("jsTest").style.color="red";
+ });
+*/
+
+
+document.getElementById("left").addEventListener("click", function(){
+     document.getElementById("Test").style.color="red";
+});
+
+
+
+
+
+/*
+ document.getElementsByClassName("sc_left").addEventListener("click", function(){
+     document.getElementsByClassName("Software_engineering").style.display="flex";
+ });
+*/
