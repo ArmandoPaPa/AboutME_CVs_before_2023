92cc152032098bbed31e794e9c80eac1b9cc8f49
ArmandoPaPa <70594270+ArmandoPaPa@users.noreply.github.com>
2020-10-03 14:41:54 +0300

    Cv web javascript 01

--- javascript.js
+++ javascript.js
@@ -1,22 +1,30 @@
-
-
-/*
-document.getElementsByClassName("sect_Container sc_left jsLeft").addEventListener("click", function(){
-     document.getElementsByClassName("jsTest").style.color="red";
- });
-*/
-
-
-document.getElementById("left").addEventListener("click", function(){
-     document.getElementById("Test").style.color="red";
+document.getElementById("jsPressStart").addEventListener("click", function(){
+    document.getElementById("jsStart").style.display="flex";
+    document.getElementById("jsProjects").style.display="none";
+    document.getElementById("jsSoft").style.display="none";
+    document.getElementById("jsEvents").style.display="none";
+    
 });
 
+document.getElementById("jsPressProjects").addEventListener("click", function(){
+    document.getElementById("jsStart").style.display="none";
+    document.getElementById("jsProjects").style.display="flex";
+    document.getElementById("jsSoft").style.display="none";
+    document.getElementById("jsEvents").style.display="none";
+//  document.getElementById("jsMainBackground").style.backgroundImage="url(../img/Conf_Small.jpg)";
 
+});
 
+document.getElementById("jsPressSoft").addEventListener("click", function(){
+    document.getElementById("jsStart").style.display="none";
+    document.getElementById("jsProjects").style.display="none";
+    document.getElementById("jsSoft").style.display="flex";
+    document.getElementById("jsEvents").style.display="none";
+});
 
-
-/*
- document.getElementsByClassName("sc_left").addEventListener("click", function(){
-     document.getElementsByClassName("Software_engineering").style.display="flex";
- });
-*/
+document.getElementById("jsPressEvents").addEventListener("click", function(){
+    document.getElementById("jsStart").style.display="none";
+    document.getElementById("jsProjects").style.display="none";
+    document.getElementById("jsSoft").style.display="none";
+    document.getElementById("jsEvents").style.display="flex";
+});
